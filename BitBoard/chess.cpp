8fb5652ddeffd8890b095d61808e4bc4a66e36c0
santacg <garciasantacarlos@gmail.com>
2024-07-16 12:08:12 +0200

    added FEN parsing

--- BitBoard/chess.cpp
+++ BitBoard/chess.cpp
@@ -25,153 +25,43 @@ Bitboard parse_fen(string fen_str) {
   int i, j, str_counter;
 
   bitset<64> white_pawns;
+  bitset<64> black_pawns;
+  bitset<64> white_rooks;
+  bitset<64> black_rooks;
+  bitset<64> white_knights;
+  bitset<64> black_knights;
+  bitset<64> white_bishops;
+  bitset<64> black_bishops;
+  bitset<64> white_queens;
+  bitset<64> black_queens;
+  bitset<64> white_king;
+  bitset<64> black_king;
+
   for (i = RANKS - 1, str_counter = 0; i >= 0; i--) {
     for (j = 0; j <= FILES; j++, str_counter++) {
       if (fen_str[str_counter] == 'P') {
         white_pawns.set((i * 8) + j, true);
-      } else if (fen_str[str_counter] == '/') {
-        str_counter++;
-        break;
-      } else if (isdigit(fen_str[str_counter]) == true) {
-        j += (fen_str[str_counter] - '0') - 1;
-      }
-    }
-  }
-
-  bitset<64> black_pawns;
-  for (i = RANKS - 1, str_counter = 0; i >= 0; i--) {
-    for (j = 0; j <= FILES; j++, str_counter++) {
-      if (fen_str[str_counter] == 'p') {
+      } else if (fen_str[str_counter] == 'p') {
         black_pawns.set((i * 8) + j, true);
-      } else if (fen_str[str_counter] == '/') {
-        str_counter++;
-        break;
-      } else if (isdigit(fen_str[str_counter]) == true) {
-        j += (fen_str[str_counter] - '0') - 1;
-      }
-    }
-  }
-  bitset<64> white_rooks;
-  for (i = RANKS - 1, str_counter = 0; i >= 0; i--) {
-    for (j = 0; j <= FILES; j++, str_counter++) {
-      if (fen_str[str_counter] == 'R') {
+      } else if (fen_str[str_counter] == 'R') {
         white_rooks.set((i * 8) + j, true);
-      } else if (fen_str[str_counter] == '/') {
-        str_counter++;
-        break;
-      } else if (isdigit(fen_str[str_counter]) == true) {
-        j += (fen_str[str_counter] - '0') - 1;
-      }
-    }
-  }
-  bitset<64> black_rooks;
-  for (i = RANKS - 1, str_counter = 0; i >= 0; i--) {
-    for (j = 0; j <= FILES; j++, str_counter++) {
-      if (fen_str[str_counter] == 'r') {
+      } else if (fen_str[str_counter] == 'r') {
         black_rooks.set((i * 8) + j, true);
-      } else if (fen_str[str_counter] == '/') {
-        str_counter++;
-        break;
-      } else if (isdigit(fen_str[str_counter]) == true) {
-        j += (fen_str[str_counter] - '0') - 1;
-      }
-    }
-  }
-  bitset<64> white_knights;
-  for (i = RANKS - 1, str_counter = 0; i >= 0; i--) {
-    for (j = 0; j <= FILES; j++, str_counter++) {
-      if (fen_str[str_counter] == 'N') {
+      } else if (fen_str[str_counter] == 'N') {
         white_knights.set((i * 8) + j, true);
-      } else if (fen_str[str_counter] == '/') {
-        str_counter++;
-        break;
-      } else if (isdigit(fen_str[str_counter]) == true) {
-        j += (fen_str[str_counter] - '0') - 1;
-      }
-    }
-  }
-  bitset<64> black_knights;
-  for (i = RANKS - 1, str_counter = 0; i >= 0; i--) {
-    for (j = 0; j <= FILES; j++, str_counter++) {
-      if (fen_str[str_counter] == 'n') {
-        black_knights.set((i * 8) + j, true);
-      } else if (fen_str[str_counter] == '/') {
-        str_counter++;
-        break;
-      } else if (isdigit(fen_str[str_counter]) == true) {
-        j += (fen_str[str_counter] - '0') - 1;
-      }
-    }
-  }
-  bitset<64> white_bishops;
-  for (i = RANKS - 1, str_counter = 0; i >= 0; i--) {
-    for (j = 0; j <= FILES; j++, str_counter++) {
-      if (fen_str[str_counter] == 'B') {
+      } else if (fen_str[str_counter] == 'n') {
+        black_pawns.set((i * 8) + j, true);
+      } else if (fen_str[str_counter] == 'B') {
         white_bishops.set((i * 8) + j, true);
-      } else if (fen_str[str_counter] == '/') {
-        str_counter++;
-        break;
-      } else if (isdigit(fen_str[str_counter]) == true) {
-        j += (fen_str[str_counter] - '0') - 1;
-      }
-    }
-  }
-  bitset<64> black_bishops;
-  for (i = RANKS - 1, str_counter = 0; i >= 0; i--) {
-    for (j = 0; j <= FILES; j++, str_counter++) {
-      if (fen_str[str_counter] == 'b') {
+      } else if (fen_str[str_counter] == 'b') {
         black_bishops.set((i * 8) + j, true);
-      } else if (fen_str[str_counter] == '/') {
-        str_counter++;
-        break;
-      } else if (isdigit(fen_str[str_counter]) == true) {
-        j += (fen_str[str_counter] - '0') - 1;
-      }
-    }
-  }
-  bitset<64> white_queens;
-  for (i = RANKS - 1, str_counter = 0; i >= 0; i--) {
-    for (j = 0; j <= FILES; j++, str_counter++) {
-      if (fen_str[str_counter] == 'Q') {
+      } else if (fen_str[str_counter] == 'Q') {
         white_queens.set((i * 8) + j, true);
-      } else if (fen_str[str_counter] == '/') {
-        str_counter++;
-        break;
-      } else if (isdigit(fen_str[str_counter]) == true) {
-        j += (fen_str[str_counter] - '0') - 1;
-      }
-    }
-  }
-  bitset<64> black_queens;
-  for (i = RANKS - 1, str_counter = 0; i >= 0; i--) {
-    for (j = 0; j <= FILES; j++, str_counter++) {
-      if (fen_str[str_counter] == 'q') {
+      } else if (fen_str[str_counter] == 'q') {
         black_queens.set((i * 8) + j, true);
-      } else if (fen_str[str_counter] == '/') {
-        str_counter++;
-        break;
-      } else if (isdigit(fen_str[str_counter]) == true) {
-        j += (fen_str[str_counter] - '0') - 1;
-      }
-    }
-  }
-  bitset<64> white_king;
-  for (i = RANKS - 1, str_counter = 0; i >= 0; i--) {
-    for (j = 0; j <= FILES; j++, str_counter++) {
-      if (fen_str[str_counter] == 'K') {
+      } else if (fen_str[str_counter] == 'K') {
         white_king.set((i * 8) + j, true);
-      } else if (fen_str[str_counter] == '/') {
-        str_counter++;
-        break;
-      } else if (isdigit(fen_str[str_counter]) == true) {
-        j += (fen_str[str_counter] - '0') - 1;
-      }
-    }
-  }
-  bitset<64> black_king;
-  for (i = RANKS - 1, str_counter = 0; i >= 0; i--) {
-    for (j = 0; j <= FILES; j++, str_counter++) {
-      if (fen_str[str_counter] == 'k') {
+      } else if (fen_str[str_counter] == 'k') {
         black_king.set((i * 8) + j, true);
       } else if (fen_str[str_counter] == '/') {
         str_counter++;
